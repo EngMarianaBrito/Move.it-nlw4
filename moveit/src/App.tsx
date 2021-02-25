b17f90751fb532bb23b950f604699fbaf7f8e925
lia mariana <marianabrito1791@gmail.com>
2021-02-25 17:33:48 -0300

    adicionando pasta do projeto inicial-iniciando front

---
+++ moveit/src/App.tsx
@@ -0,0 +1,13 @@
+import { ExperienceBar } from './componentes/ExperienceBar';
+
+import './styles/global.css';
+
+function App() {
+  return (
+   <div className="container">
+      <ExperienceBar />
+   </div>
+  );
+}
+
+export default App;
